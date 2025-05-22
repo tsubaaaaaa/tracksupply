ce21f9df82e1e7e9763feb1edeb6ec2ba5f5b3ea
tsubaaaaaa <Ilovemake2886@gmail.com>
2025-05-23 01:19:08 +0900

    migration-update-syntaxerror

--- db/migrate/20250420170113_create_group_users.rb
+++ db/migrate/20250420170113_create_group_users.rb
@@ -1,13 +1,14 @@
 class CreateGroupUsers < ActiveRecord::Migration[7.1]
   def change
     unless table_exists?(:group_users)
-    create_table :group_users do |t|
-      t.references :user, null: false, foreign_key: true
-      t.references :group, null: false, foreign_key: true
-      t.string :role, null: false, default: "member" # Default role is "member"
+      create_table :group_users do |t|
+        t.references :user, null: false, foreign_key: true
+        t.references :group, null: false, foreign_key: true
+        t.string :role, null: false, default: "member" # Default role is "member"
 
-      t.timestamps
+        t.timestamps
 
+      end
     end
 
     add_index :group_users, [:user_id, :group_id], unique: true
