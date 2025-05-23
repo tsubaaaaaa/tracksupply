4711c7cb502c13586f3cfdbc6ba986bb45c74923
tsubaaaaaa <Ilovemake2886@gmail.com>
2025-05-23 13:32:42 +0900

    uodate-manifest.js

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,6 +1,3 @@
+// app/assets/config/manifest.js
 //= link_tree ../images
-//= link_directory ../stylesheets .css
-//= link_tree ../../javascript .js
-//= link_tree ../../../vendor/javascript .js
-
-//=link homes/index.css
+//= link_directory ../stylesheets .css // または .scss を使っていれば .scss も対象にするか、application.css にまとめる
