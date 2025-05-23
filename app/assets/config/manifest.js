b4f98698ada7ab19bb887492b70e68d13b5a2d15
tsubaaaaaa <Ilovemake2886@gmail.com>
2025-05-23 17:23:52 +0900

    uuuu

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,10 @@
 // app/assets/config/manifest.js
 //= link_tree ../images
 //= link_directory ../stylesheets .css 
+
+//= link application.js
+//= link errors.js
+//= link add-column.js
+//= link controllers/application.js
+//= link controllers/hello_controller.js
+//= link controllers/index.js
--- config/importmap.rb
+++ config/importmap.rb
@@ -7,6 +7,3 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
-
-pin "errors", to: "errors.js"
-pin "add-column", to: "add-column.js"
