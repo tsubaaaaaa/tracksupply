b2c0fd1ed37198a123581155ba2d1abe7832c870
tsubaaaaaa <Ilovemake2886@gmail.com>
2025-05-23 13:39:16 +0900

    mani.js-ip

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,4 @@
 // app/assets/config/manifest.js
 //= link_tree ../images
-//= link_directory ../stylesheets .css // または .scss を使っていれば .scss も対象にするか、application.css にまとめる
+//= link_directory ../stylesheets .css
+//
