0f38b717c88ba0e817f09426d8441d44c4e1e140
tsubaaaaaa <Ilovemake2886@gmail.com>
2025-05-23 16:58:51 +0900

    manifest.js-changed

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,7 +1,3 @@
 // app/assets/config/manifest.js
 //= link_tree ../images
 //= link_directory ../stylesheets .css 
-
-//= link controllers/application.js
-//= link controllers/hello_controller.js
-//= link controllers/index.js
