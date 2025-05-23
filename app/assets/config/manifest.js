8ebf440896303f1750a2e4fbfec2786e1c1b4ec2
tsubaaaaaa <Ilovemake2886@gmail.com>
2025-05-23 14:26:43 +0900

    worked

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,8 @@
 // app/assets/config/manifest.js
 //= link_tree ../images
-//= link_directory ../stylesheets .css
-//
+//= link_directory ../stylesheets .css 
+
+//= link application.js
+//= link controllers/application.js
+//= link controllers/hello_controller.js
+//= link controllers/index.js
