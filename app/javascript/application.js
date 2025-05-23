eab3cbed1bd21636d8964c4796345646271667e1
tsubaaaaaa <Ilovemake2886@gmail.com>
2025-05-23 13:50:04 +0900

    test

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+//import "@hotwired/turbo-rails"
+//import "controllers"
 
-import "./errors.js"
+//import "./errors.js"
 
+console.log("Application.js loaded!")
